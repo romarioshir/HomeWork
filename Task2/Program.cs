52c47b4537ac2554052c7b0688c0a4d21433e8b9
romarioshir <roma-roman26@yandex.ru>
2022-10-02 00:22:29 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+
+Console.WriteLine("Введите целое число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int b = int.Parse(userInput2);
+
+if (a > b)
+{
+Console.WriteLine("a = " + a + " , b = " + b + " , max = " + a);
+}
+else
+{
+Console.WriteLine("a = " + a + " , b = " + b + " , max = " + b);
+}
