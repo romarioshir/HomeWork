db7b3ae3735ea6711f9ad29738eb31bb5d2f1387
romarioshir <roma-roman26@yandex.ru>
2022-10-02 01:06:34 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+
+if (a % 2 == 0)
+{
+Console.WriteLine(a + " -> да");
+}
+else
+{
+Console.WriteLine(a + " -> нет");
+}
