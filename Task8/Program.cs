f2c66863aa4ea5e49e7659218bd1f15fef0300c2
romarioshir <roma-roman26@yandex.ru>
2022-10-02 01:44:00 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int a = int.Parse(userInput);
+
+int count = 1;
+
+Console.Write(a + " -> ");
+
+while (count < a + 1)
+{
+    if (count % 2 == 0)
+    {
+    Console.Write(count + " "); //Прошу пояснения в комментарии по поводу этой строчки, а именно что делает и почему работает (переменная + " ") ?
+    }
+    count++;
+
+}
